74361f93bd5f01fb49719ec0c811be164df04945
Zheng Hao <hao.zheng@amlogic.com>
2022-11-13 21:53:52 +0800

    common: config for new aml_npu_common restority [1/7]
    PD#SWPL-96359
    
    Problem:
    add npu common repository
    
    Solution:
    add the config
    
    Verify:
    local
    
    Change-Id: I172529bf09e38ae500e5016c27c3d90e46e24f26
    Signed-off-by: Zheng Hao <hao.zheng@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -1,39 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_ARCH), arm64)
-SDK_PATH=libraryso/lib64
-Target=lib64
-else
-SDK_PATH=libraryso/lib32
-Target=lib
-endif
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-    $(SDK_PATH)/libnnsdk.so
-ifeq ($(PRODUCT_CHIP_ID), ADLA_S5)
-LOCAL_MODULE         := libnnsdk
-else
-ifeq ($(PRODUCT_CHIP_ID), ADLA_T7)
-LOCAL_MODULE         := libnnsdk
-else
-LOCAL_MODULE         := libnnsdk_bak
-endif
-endif
-LOCAL_LICENSE_KINDS := SPDX-license-identifier-GPL-2.0+
-LOCAL_LICENSE_CONDITIONS := notice
-LOCAL_MODULE_SUFFIX  := .so
-LOCAL_MODULE_TAGS    := optional
-LOCAL_MODULE_CLASS   := SHARED_LIBRARIES
-LOCAL_CHECK_ELF_FILES := false
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/$(Target)
-else
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-endif
-include $(BUILD_PREBUILT)
-
 SERVICE_PATH=$(LOCAL_PATH)/service
 ifeq ($(BOARD_ADLA_SERVICE_ENABLE), true)
 include $(SERVICE_PATH)/Android.mk
